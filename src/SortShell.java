be7757d423e4d9b49ac9bd1e4bd32f58de0999b3
Xavier Fajardo <fajardoxavier61@gmail.com>
2025-10-28 19:12:58 -0500

    M. Shell

--- src/SortShell.java
+++ src/SortShell.java
@@ -6,23 +6,30 @@ public class SortShell {
     public void sort(int[] numeros, boolean ase){
         int n= numeros.length;
         int comparacion=0;
+        int cambios =0;
         for(int gap = n/2;gap>0;gap/=2){
-            int cambios =0;
             for(int i=gap;i<n;i++){
                 int temp = numeros[i];
                 int j =i;
-                while(j>=gap && numeros[j-gap]> temp){
-                    comparacion++;
-                    numeros[j]=numeros[j-gap];
-                    j-=gap;
-                    cambios++;
-                }
-                numeros[j]=temp;
+                while (j >= gap) {
+                    if (numeros[j - gap] > temp) {
+                        comparacion++;
+                        numeros[j] = numeros[j - gap];
+                        j -= gap;
+                        cambios++;                    
+                    }else{
+                        comparacion++;
+                        break;
+                    }   
+                    
+                }  
+                numeros[j] = temp;
+               
             }
 
         }
-        int total = comparacion ;
-        System.out.println("\nNumero de comparaciones: " + total);
+        int total = comparacion + cambios;
+        System.out.println("\n(Numero de comparaciones y cambio): " + total);
 
     }
     
